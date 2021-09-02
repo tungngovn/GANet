3e5e49d99a413704a835d2078c81b6dcedb4a545
tungngovn <tung.ngothanh.hust@gmail.com>
2021-09-02 19:36:35 +0700

    Add evaluation of Apolloscape dataset

--- bash_scripts/apolloscape_evaluation.sh
+++ bash_scripts/apolloscape_evaluation.sh
@@ -1,10 +1,10 @@
-CUDA_VISIBLE_DEVICES=0 python evaluation.py --crop_height=384 \
-                  --crop_width=1248 \
+CUDA_VISIBLE_DEVICES=0 python evaluation_apolloscape.py --crop_height=288 \
+                  --crop_width=576 \
                   --max_disp=192 \
                   --data_path='./dataset/apolloscape/' \
                   --test_list='lists/apolloscape_test.list' \
                   --save_path='./result/apolloscape/' \
-                  --resume='./checkpoint/apolloscape_final.pth' \
+                  --resume='./checkpoint/_epoch_49.pth' \
                   --threshold=3.0
 2>&1 |tee logs/log_evaluation.txt
 exit
