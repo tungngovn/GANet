f0815733daea780c002a0876ef9ac608aceb3aca
tungngovn <tung.ngothanh.hust@gmail.com>
2021-09-02 19:43:25 +0700

    Edit path to checkpoint

--- bash_scripts/apolloscape_evaluation.sh
+++ bash_scripts/apolloscape_evaluation.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=0 python evaluation_apolloscape.py --crop_height=288 \
                   --data_path='./dataset/apolloscape/' \
                   --test_list='lists/apolloscape_test.list' \
                   --save_path='./result/apolloscape/' \
-                  --resume='./checkpoint/_epoch_49.pth' \
+                  --resume='./checkpoint/apolloscape_check/_epoch_49.pth' \
                   --threshold=3.0
 2>&1 |tee logs/log_evaluation.txt
 exit
